11ef92454fcd5e94a4462c78b2bde9d7cf5fc5d0
ZAIN <97236509+redhatzain@users.noreply.github.com>
2022-02-14 11:25:07 -0800

    Add files via upload

---
+++ DECRYPT.py
@@ -0,0 +1,11 @@
+## CREATED-BY-REDHATZAIN
+## GITHUB : REDHATZAIN
+## BY CHANGIN CODE YOU WILL NOT BECOME REDHATZAIN
+import base64, codecs
+magic = 'aW1wb3J0IHN0cmluZw0KcGxhaW5fdGV4dCA9IGlucHV0KCJFTlRFUiBFTkNSWVBURUQgVEVYVCA6ICIpDQpzaGl'
+love = 'zqPN9VQV2YGpAPzSfpTuuLzI0VQ0tp3ElnJ5aYzSmL2ycK2kiq2IlL2SmMD0Xp2ucMaEyMPN9LJkjnTSvMKEop2'
+god = 'hpZnQ6XSArIGFscGhhYmV0WzpzaGlmdF0NCnRhYmxlID0gc3RyLm1ha2V0cmFucyhhbHBoYWJldCxzaGlmdGVkK'
+destiny = 'D0XQDcyozAlrKO0MJDtCFOjoTScoy90MKu0YaElLJ5moTS0MFu0LJWfMFxAPaOlnJ50XTIhL3W5pUEyMPxAPt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
---
+++ ENCRYPT.py
@@ -0,0 +1,11 @@
+## CREATED-BY-REDHATZAIN
+## GITHUB : REDHATZAIN
+## BY CHANGIN CODE YOU WILL NOT BECOME REDHATZAIN
+import base64, codecs
+magic = 'aW1wb3J0IHN0cmluZw0KcGxhaW5fdGV4dCA9IGlucHV0KCJFbnRlciBUZXh0IFlvdSBXYW50IFRvIEVuY3J5cHQgO'
+love = 'vNvXD0Xp2ucMaDtCFN3QDcuoUObLJWyqPN9VUA0pzyhMl5up2AcnI9fo3qypzAup2HAPaAbnJM0MJDtCJSfpTuuLz'
+god = 'V0W3NoaWZ0Ol0gKyBhbHBoYWJldFs6c2hpZnRdDQp0YWJsZSA9IHN0ci5tYWtldHJhbnMoYWxwaGFiZXQsc2hpZnR'
+destiny = 'yMPxAPt0XMJ5wpayjqTIxVQ0tpTkunJ5sqTI4qP50pzShp2kuqTHbqTSvoTHcQDcjpzyhqPuyozAlrKO0MJDcQDb='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
